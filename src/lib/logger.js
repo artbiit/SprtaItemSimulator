02e1007d5c0b361f97884e0e860511c0e3675277
ljs <artbiit@naver.com>
2024-09-06 11:15:21 +0900

    logger 추가

---
+++ src/lib/logger.js
@@ -0,0 +1,30 @@
+import winston from 'winston';
+import _ from 'winston-daily-rotate-file';
+
+// 로그 형식 정의
+const logFormat = winston.format.printf(({ timestamp, level, message }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
+
+// 로그 파일을 매일 새로 생성하고, 5일간만 보관
+const transport = new winston.transports.DailyRotateFile({
+    filename: 'logs/application-%DATE%.log',
+    datePattern: 'YYYY-MM-DD-HH-mm-ss',
+    maxFiles: '5d',  // 5일간의 로그 파일만 유지
+    zippedArchive: true,  // 로그 파일 압축
+});
+
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        logFormat
+    ),
+    transports: [
+        transport,
+        new winston.transports.Console(), 
+    ],
+});
+
+export default logger;
