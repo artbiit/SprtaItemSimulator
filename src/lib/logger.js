97ff82ff44f271561f64c020e7af527af76214f2
ljs <artbiit@naver.com>
2024-09-09 19:16:02 +0900

    로그 시간 단위로 보관하게 변경

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -9,7 +9,7 @@ const logFormat = winston.format.printf(({ timestamp, level, message }) => {
 // 로그 파일을 매일 새로 생성하고, 5일간만 보관
 const transport = new winston.transports.DailyRotateFile({
   filename: 'logs/application-%DATE%.log',
-  datePattern: 'YYYY-MM-DD-HH-mm-ss',
+  datePattern: 'YYYY-MM-DD-HH',
   maxFiles: '5d', // 5일간의 로그 파일만 유지
   zippedArchive: true, // 로그 파일 압축
 });
